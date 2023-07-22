48c0cf32c023afc774fc70e18ae4b6ea9e74ed99
Mordekai_HD <evgeni.kap@list.ru>
2023-07-22 16:16:38 +0300

    Task-41

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void ShowArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Кол-во элементов больше 0: {count}");
+}
+
+Console.Write("Введите числа для проверки (через запятую): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(','),int.Parse);
+ShowArray(array);
+
+
+ 
+
+
+
+
